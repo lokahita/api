fd50f719d33c307bccbe639ebb6889b77ef19adb
Muhammad Hasannudin Yusa <emhayusa@gmail.com>
2021-08-06 11:59:59 +0700

    add end point update data to geoserver

--- app/main/controller/data_controller.py
+++ app/main/controller/data_controller.py
@@ -15,8 +15,8 @@ api = DataDto.api
 
 #APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'data', 'uploads')
-GEOSERVER = "http://localhost/geoserver/rest/" #"https://data.cifor.org/geoportal/geoserver/rest/"
-AUTH = "Basic YWRtaW46Z2Vvc2VydmVy" #'Authorization: Basic YWRtaW46bXlhd2Vzb21lZ2Vvc2VydmVy' 
+GEOSERVER = "https://data.cifor.org/geoportal/geoserver/rest/" #"http://localhost/geoserver/rest/"
+AUTH = "Authorization: Basic YWRtaW46bXlhd2Vzb21lZ2Vvc2VydmVy" #'Authorization: Basic YWRtaW46bXlhd2Vzb21lZ2Vvc2VydmVy' 
 WS = 'fta'
 CONTENT = "Content-type: application/zip"
 
@@ -82,7 +82,68 @@ def upload_geoserver(filename, username):
                     }
     return response_object, 201
 
-    
+
+@api.route('/update/')
+class UpdateDataDtoList(Resource):
+    #@api.doc('list of metadata')
+    #@api.marshal_list_with(_schema, envelope='data')
+    #@admin_token_required
+    #def get(self):
+    #    """List all metadata"""
+    #    return get_all()
+
+    @api.response(201, 'Data successfully uploaded.')
+    @api.doc('upload a new data')
+    #@api.expect(_entry, validate=True)
+    #@admin_token_required
+    @token_required
+    def post(self):
+        """Uploads a new Data """
+        #data = request.json
+
+        file = request.files['file']
+        #username= request['username']
+        print(file)
+        #print(request.__dict__)
+        #print(request.form['username'])
+        username = request.form['username']
+        #shapeFile = request.form['shapeFile']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            response_object = {
+            'status': 'error',
+            'message': 'Select file first'
+            }
+            return response_object, 200
+        else:
+            file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+            return update_geoserver(file.filename, username)
+
+def update_geoserver(filename, username):
+    print(filename, username)
+    print(filename.replace('.zip', ''))
+    result = subprocess.run([
+    'curl', 
+    '-v', 
+    '-XPUT', 
+    '-H',
+    AUTH,
+    '-H',
+    CONTENT,
+    '--data-binary',
+    '@'+os.path.join(UPLOAD_FOLDER,filename),
+    GEOSERVER + 'workspaces/'+WS+'/datastores/'+ filename.replace('.zip', '_emhayusa') +'/file.shp?update=overwrite'
+    ], stdout=subprocess.PIPE)
+    print(result)
+    print(result.stdout.decode('utf-8'))
+    #url_store = GEOSERVER + 'workspaces/'+WS+'/datastores/' + username + '.json'
+    response_object = {
+                        'status': 'success',
+                        'message': 'Successfully updated'
+                    }
+    return response_object, 201
+
 
 @api.route('/publish/')
 class DataPublish(Resource):
